502a5202886f0615ac1d2148ceff42d66ec82db1
AntonLihtar <sa-fa@mail.ru>
2022-11-05 23:19:24 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+    private String konec = "";
+
+    @Override
+    public String toString() {
+        return  konec;
+    }
+}
